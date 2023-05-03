70dee50a242c745e3e01650eebb00c8c5d11480a
Khalim Conn-Kowlessar <kconnkowlessar@gmail.com>
2023-05-03 16:44:50 +0100

    BEgin parsing api response

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,8 +1,35 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { NextPageWithLayout } from "./_app";
 import Layout from "~/components/layout";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { GetStaticProps } from "next";
+import { SearchResults } from "~/types";
 
-const Results: NextPageWithLayout = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const url = "https://epc.opendatacommunities.org/api/v1/domestic/search";
+
+  const headers = new Headers({
+    Accept: "application/json",
+    Authorization: "Basic " + process.env.EPC_AUTH_TOKEN,
+    "Content-Type": "application/json",
+  });
+
+  console.log(headers);
+
+  const request = await fetch(url, { headers: headers });
+  const data = await request.json();
+
+  return {
+    props: { data: data },
+  };
+};
+
+const Results: NextPageWithLayout = (data: SearchResults) => {
+  const router = useRouter();
+
+  const postcode = router.query.postcode;
+
   return (
     <>
       <h1>Results</h1>
